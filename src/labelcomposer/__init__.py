6485637f4f877e70604898cc83352aa9a5cd6f31
Larissa Heinrich <heinrichl@janelia.hhmi.org>
2023-06-08 16:26:32 -0400

    feat: :art: make classes avaialble at package level import

--- src/labelcomposer/__init__.py
+++ src/labelcomposer/__init__.py
@@ -1,3 +1,4 @@
 # SPDX-FileCopyrightText: 2023-present Larissa Heinrich <heinrichl@janelia.hhmi.org>
 #
 # SPDX-License-Identifier: MIT
+from labelcomposer.label import AtomicLabel, Label, LabelCollection
